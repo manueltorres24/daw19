b897ac0528f2661e8523498a1c0fa520b94ed949
Manuel <Manuel@Msito>
2020-02-18 19:52:31 +0100

    Primera version, segunda linea.

--- src/daw19/Daw19.java
+++ src/daw19/Daw19.java
@@ -7,6 +7,7 @@ public class Daw19 {
 
     public static void main(String[] args) {
      System.out.println("Hola mundo maravilloso");
+     System.out.println("Adios mundo");
         
         
     }
